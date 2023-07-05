660cec4f010f7e75800d2b9a62f4e943e2131c90
Đinh Viết Quân <104368461+Quann111@users.noreply.github.com>
2023-07-05 11:20:29 +0700

    Add project files.

---
+++ BookShoppingCartMvcUI/Areas/Admin/Controllers/Admin.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShoppingCartMvcUI.Areas.Admin.Controllers
+{
+    public class Admin : Controller
+    {
+        [Area("Admin")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookShoppingCartMvcUI/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookShoppingCartMvcUI.Data;
+using BookShoppingCartMvcUI.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookShoppingCartMvcUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Books
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Books.Include(b => b.Genre);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Genre)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Admin/Books/Create
+        public IActionResult Create()
+        {
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "GenreName");
+            return View();
+        }
+
+        // POST: Admin/Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,BookName,AuthorName,Price,Image,GenreId")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "GenreName", book.GenreId);
+            return View(book);
+        }
+
+        // GET: Admin/Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "GenreName", book.GenreId);
+            return View(book);
+        }
+
+        // POST: Admin/Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BookName,AuthorName,Price,Image,GenreId")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreId"] = new SelectList(_context.Genres, "Id", "GenreName", book.GenreId);
+            return View(book);
+        }
+
+        // GET: Admin/Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Genre)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Admin/Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Books == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Books'  is null.");
+            }
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+          return (_context.Books?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
